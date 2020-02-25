1c7a21865a15b2090be548b052bab86f61416b1c
Amadeus Folego <amadeusfolego@gmail.com>
2020-02-25 18:07:20 -0300

    Make badmod hosts compatible with normal game

---
+++ gui/gamesetup/gamesetup~badmod.js
@@ -0,0 +1,57 @@
+if (!global["getFilteredMods"])
+  global["getFilteredMods"] = function () { return Engine.GetEngineInfo().mods };
+
+autociv_patchApplyN("getFilteredMods", function (target, that, args)
+{
+	let mod = ([name, version]) => !/^FGod.*/i.test(name);
+	return target.apply(that, args).filter(mod);
+});
+
+autociv_patchApplyN("getFilteredMods", function (target, that, args)
+{
+	let mod = ([name, version]) => !/^AutoCiv.*/i.test(name);
+	return target.apply(that, args).filter(mod);
+});
+
+autociv_patchApplyN("getFilteredMods", function (target, that, args)
+{
+	let mod = ([name, version]) => !/^badmod.*/i.test(name);
+	return target.apply(that, args).filter(mod);
+});
+
+sendRegisterGameStanzaImmediate = function ()
+{
+	if (!g_IsController || !Engine.HasXmppClient())
+		return;
+
+	if (g_GameStanzaTimer !== undefined)
+	{
+		clearTimeout(g_GameStanzaTimer);
+		g_GameStanzaTimer = undefined;
+	}
+
+	let clients = formatClientsForStanza();
+	let stanza = {
+		"name": g_ServerName,
+		"port": g_ServerPort,
+		"hostUsername": Engine.LobbyGetNick(),
+		"mapName": g_GameAttributes.map,
+		"niceMapName": getMapDisplayName(g_GameAttributes.map),
+		"mapSize": g_GameAttributes.mapType == "random" ? g_GameAttributes.settings.Size : "Default",
+		"mapType": g_GameAttributes.mapType,
+		"victoryConditions": g_GameAttributes.settings.VictoryConditions.join(","),
+		"nbp": clients.connectedPlayers,
+		"maxnbp": g_GameAttributes.settings.PlayerData.length,
+		"players": clients.list,
+		"stunIP": g_StunEndpoint ? g_StunEndpoint.ip : "",
+		"stunPort": g_StunEndpoint ? g_StunEndpoint.port : "",
+		"mods": JSON.stringify(getFilteredMods()) // <----- THIS CHANGES
+	};
+
+	// Only send the stanza if the relevant settings actually changed
+	if (g_LastGameStanza && Object.keys(stanza).every(prop => g_LastGameStanza[prop] == stanza[prop]))
+		return;
+
+	g_LastGameStanza = stanza;
+	Engine.SendRegisterGame(stanza);
+};
