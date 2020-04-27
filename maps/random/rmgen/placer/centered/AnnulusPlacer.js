948d3688f59a72b78b6d82db5cdc38790787e551
Amadeus Folego <amadeusfolego@gmail.com>
2020-04-26 22:11:35 -0300

    Optimize AnnulusPlacer

--- maps/random/rmgen/placer/centered/AnnulusPlacer.js
+++ maps/random/rmgen/placer/centered/AnnulusPlacer.js
@@ -16,20 +16,24 @@ AnnulusPlacer.prototype.setCenterPosition = function(position)
 
 AnnulusPlacer.prototype.place = function(constraint)
 {
-  let points = [];
-
   const minX = this.centerPosition.x - this.maxRadius;
   const maxX = this.centerPosition.x + this.maxRadius;
   const minY = this.centerPosition.y - this.maxRadius;
   const maxY = this.centerPosition.y + this.maxRadius;
 
+  let points = [];
+  let point = new Vector2D();
   for (let x = minX; x <= maxX; ++x)
     for (let y = minY; y <= maxY; ++y)
     {
-      const point = new Vector2D(x, y);
+      point.set(x, y);
+
+      if (!g_Map.validTile(point)) { continue; }
+
       const distance = this.centerPosition.distanceToSquared(point);
+
       if (this.minRadiusSquared <= distance && this.maxRadiusSquared >= distance && constraint.allows(point))
-        points.push(point);
+        points.push(point.clone());
     }
 
   return points;
