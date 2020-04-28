b8952e7457308e570b9efdad2aa54028c0ca0379
Amadeus Folego <amadeusfolego@gmail.com>
2020-04-27 21:15:41 -0300

    Create initial compatibility matcher

--- gui/common/mod~badmod.js
+++ gui/common/mod~badmod.js
@@ -1,3 +1,5 @@
+global["balancedMapsCompatibilityMatcher"] = new RegExp('^0\\.18(\\.|$)', 'i');
+
 autociv_patchApplyN("hasSameMods", function (target, that, args)
 {
 	let mod = ([name, version]) => !/^FGod.*/i.test(name);
@@ -12,6 +14,12 @@ autociv_patchApplyN("hasSameMods", function (target, that, args)
 
 autociv_patchApplyN("hasSameMods", function (target, that, args)
 {
-	let mod = ([name, version]) => !/^balanced[-_]maps.*/i.test(name);
-	return target.apply(that, args.map(mods => mods.filter(mod)));
+  let nameFilter = ([name]) => !/^balanced[-_]maps.*/i.test(name);
+  let compatibilityFilter = ([name, version]) => (nameFilter(name) || global["balancedMapsCompatibilityMatcher"].test(version));
+
+  if (args[0].every(compatibilityFilter)) {
+    return target.apply(that, args.map(mods => mods.filter(nameFilter)));
+  } else {
+    return target.apply(that, args);
+  }
 })
--- gui/gamesetup/gamesetup~badmod.js
+++ gui/gamesetup/gamesetup~badmod.js
@@ -1,57 +1,74 @@
-if (!global["getFilteredMods"])
-  global["getFilteredMods"] = function () { return Engine.GetEngineInfo().mods };
-
-autociv_patchApplyN("getFilteredMods", function (target, that, args)
+function badmod_patchModFilter()
 {
-	let mod = ([name, version]) => !/^FGod.*/i.test(name);
-	return target.apply(that, args).filter(mod);
-});
+  if (!global["getFilteredMods"])
+    global["getFilteredMods"] = function(gameData) { return Engine.GetEngineInfo().mods };
 
-autociv_patchApplyN("getFilteredMods", function (target, that, args)
+  if (!global["balancedMaps"])
+    global["balancedMaps"] = ['maps/random/wrench', 'maps/random/badmainland', 'maps/random/bad_hyrcanian_shores', 'maps/random/cross'];
+
+  function isBalancedMap(mapName) {
+    return global["balancedMaps"].indexOf(mapName) > -1;
+  }
+
+  autociv_patchApplyN("getFilteredMods", function (target, that, args)
+  {
+  	let mod = ([name, version]) => !/^FGod.*/i.test(name);
+  	return target.apply(that, args).filter(mod);
+  });
+
+  autociv_patchApplyN("getFilteredMods", function (target, that, args)
+  {
+  	let mod = ([name, version]) => !/^AutoCiv.*/i.test(name);
+  	return target.apply(that, args).filter(mod);
+  });
+
+  autociv_patchApplyN("getFilteredMods", function (target, that, args)
+  {
+  	let mod = ([name, version]) => (!/^balanced[-_]maps.*/i.test(name) || isBalancedMap(args[0].map));
+
+  	return target.apply(that, args).filter(mod);
+  });
+  
+  sendRegisterGameStanzaImmediate = function ()
+  {
+  	if (!g_IsController || !Engine.HasXmppClient())
+  		return;
+  
+  	if (g_GameStanzaTimer !== undefined)
+  	{
+  		clearTimeout(g_GameStanzaTimer);
+  		g_GameStanzaTimer = undefined;
+  	}
+  
+  	let clients = formatClientsForStanza();
+  	let stanza = {
+  		"name": g_ServerName,
+  		"port": g_ServerPort,
+  		"hostUsername": Engine.LobbyGetNick(),
+  		"mapName": g_GameAttributes.map,
+  		"niceMapName": getMapDisplayName(g_GameAttributes.map),
+  		"mapSize": g_GameAttributes.mapType == "random" ? g_GameAttributes.settings.Size : "Default",
+  		"mapType": g_GameAttributes.mapType,
+  		"victoryConditions": g_GameAttributes.settings.VictoryConditions.join(","),
+  		"nbp": clients.connectedPlayers,
+  		"maxnbp": g_GameAttributes.settings.PlayerData.length,
+  		"players": clients.list,
+  		"stunIP": g_StunEndpoint ? g_StunEndpoint.ip : "",
+  		"stunPort": g_StunEndpoint ? g_StunEndpoint.port : "",
+  		"mods": JSON.stringify(getFilteredMods(g_GameAttributes)) // <----- THIS CHANGES
+  	};
+  
+  	// Only send the stanza if the relevant settings actually changed
+  	if (g_LastGameStanza && Object.keys(stanza).every(prop => g_LastGameStanza[prop] == stanza[prop]))
+  		return;
+  
+  	g_LastGameStanza = stanza;
+  	Engine.SendRegisterGame(stanza);
+  };
+}
+
+autociv_patchApplyN("init", function (target, that, args)
 {
-	let mod = ([name, version]) => !/^AutoCiv.*/i.test(name);
-	return target.apply(that, args).filter(mod);
-});
-
-autociv_patchApplyN("getFilteredMods", function (target, that, args)
-{
-	let mod = ([name, version]) => !/^balanced[-_]maps.*/i.test(name);
-	return target.apply(that, args).filter(mod);
-});
-
-sendRegisterGameStanzaImmediate = function ()
-{
-	if (!g_IsController || !Engine.HasXmppClient())
-		return;
-
-	if (g_GameStanzaTimer !== undefined)
-	{
-		clearTimeout(g_GameStanzaTimer);
-		g_GameStanzaTimer = undefined;
-	}
-
-	let clients = formatClientsForStanza();
-	let stanza = {
-		"name": g_ServerName,
-		"port": g_ServerPort,
-		"hostUsername": Engine.LobbyGetNick(),
-		"mapName": g_GameAttributes.map,
-		"niceMapName": getMapDisplayName(g_GameAttributes.map),
-		"mapSize": g_GameAttributes.mapType == "random" ? g_GameAttributes.settings.Size : "Default",
-		"mapType": g_GameAttributes.mapType,
-		"victoryConditions": g_GameAttributes.settings.VictoryConditions.join(","),
-		"nbp": clients.connectedPlayers,
-		"maxnbp": g_GameAttributes.settings.PlayerData.length,
-		"players": clients.list,
-		"stunIP": g_StunEndpoint ? g_StunEndpoint.ip : "",
-		"stunPort": g_StunEndpoint ? g_StunEndpoint.port : "",
-		"mods": JSON.stringify(getFilteredMods()) // <----- THIS CHANGES
-	};
-
-	// Only send the stanza if the relevant settings actually changed
-	if (g_LastGameStanza && Object.keys(stanza).every(prop => g_LastGameStanza[prop] == stanza[prop]))
-		return;
-
-	g_LastGameStanza = stanza;
-	Engine.SendRegisterGame(stanza);
-};
+	target.apply(that, args);
+	badmod_patchModFilter();
+})
